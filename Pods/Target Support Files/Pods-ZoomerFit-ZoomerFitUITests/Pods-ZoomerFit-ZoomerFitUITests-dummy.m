4c489918cd8f347e2cdcc3b09228cc461faae9e8
Ryan Cho <ryanmcho@gmail.com>
2020-04-26 22:36:40 -0400

    Add files via upload
    Integrated app with Firebase.

---
+++ Pods/Target Support Files/Pods-ZoomerFit-ZoomerFitUITests/Pods-ZoomerFit-ZoomerFitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZoomerFit_ZoomerFitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ZoomerFit_ZoomerFitUITests
+@end
---
+++ Pods/Target Support Files/Pods-ZoomerFit/Pods-ZoomerFit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZoomerFit : NSObject
+@end
+@implementation PodsDummy_Pods_ZoomerFit
+@end
