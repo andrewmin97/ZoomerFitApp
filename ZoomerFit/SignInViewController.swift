5303fd1beeb4cb64a9c00083e3109e123b5429c8
Ryan Cho <ryanmcho@gmail.com>
2020-04-29 22:50:01 -0400

    Facebook Working with Firebase
    Got Faceook to authenticate with Firebase.  Still need to set up better UI and segue.

--- ZoomerFit/SignInViewController.swift
+++ ZoomerFit/SignInViewController.swift
@@ -9,34 +9,53 @@
 import UIKit
 import FirebaseUI
 import FBSDKLoginKit
+import Firebase
 
 class ViewController: UIViewController, LoginButtonDelegate {
-    func loginButton(_ loginButton: FBLoginButton!, didCompleteWith result: LoginManagerLoginResult!, error: Error!) {
-      if let error = error {
-        print(error.localizedDescription)
-        return
-      }
-      let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
-    
-    }
-    
-    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-    }
         
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let token = AccessToken.current,
-            !token.isExpired {
-            // User is logged in, do work such as go to next view controller.
-        }
+    
         let loginButton = FBLoginButton()
         loginButton.delegate = self
         loginButton.center = view.center
         view.addSubview(loginButton)
         loginButton.permissions = ["public_profile", "email"]
+        if let token = AccessToken.current,
+            !token.isExpired {
+            firebaseFBLogin(accessToken: AccessToken.current!.tokenString)
+        }
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+    }
+    
+    func loginButton(_ loginButton: FBLoginButton!, didCompleteWith result: LoginManagerLoginResult!, error: Error!) {
+      if let error = error {
+        print(error.localizedDescription)
+        return
+      }
+    }
+    
+    func firebaseFBLogin(accessToken: String) {
+        let credential = FacebookAuthProvider.credential(withAccessToken: accessToken)
+        Auth.auth().signInAndRetrieveData(with: credential, completion: {(authResult, error) in
+            if let error = error {
+                print("Firebase Login Error")
+                print(error)
+                return
+            }
+            print("Firebase Login Done")
+            print(authResult)
+            if let user = Auth.auth().currentUser {
+                print("current firebase user is")
+                print(user)
+            }
+        })
+        performSegue(withIdentifier: "toTab", sender: self)
     }
 
     @IBAction func SignInPressed(_ sender: Any) {
